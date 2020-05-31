84c000271ce1705574b4a7bb1800edf1b3d75d4b
arogozine <alexandre.rogozine@live.com>
2020-05-31 14:21:19 -0600

    Generate NuGet package on build

--- EnumUtilTests/Performance.cs
+++ EnumUtilTests/Performance.cs
@@ -12,169 +12,167 @@ namespace EnumUtilTests
         // For methods rewritten, the performance is expected to be higher
         // than the default implementation
 
-        public static long IsDefinedStandard(out int i)
-        {
-            var watch = Stopwatch.StartNew();
-
-            bool isDefined = true;
-
-            i = 0;
-            foreach (int val in EnumUtil<Int32Enum>.GetValues())
-            {
-                for (i = 0; i < 200000; i++)
-                {
-                    isDefined &= Enum.IsDefined(typeof(Int32Enum), val);
-                }
-            }
-
-            watch.Stop();
-            Assert.IsTrue(isDefined);
-            return watch.ElapsedMilliseconds;
-        }
-
-        public static long IsDefinedGeneric(out int i)
-        {
-            var watch = Stopwatch.StartNew();
-
-            bool isDefined = true;
-
-            var isDefinedFunc =
-
-            i = 0;
-            foreach (byte val in EnumUtil<Int32Enum>.GetValues())
-            {
-                for (i = 0; i < 200000; i++)
-                {
-                    isDefined &= EnumUtil<Int32Enum>.IsDefined(val);
-                }
-            }
-
-            watch.Stop();
-            Assert.IsTrue(isDefined);
-            return watch.ElapsedMilliseconds;
-        }
-
         [TestMethod]
         public void IsDefinedNumericPerformance()
         {
-            int i;
             // warmup
-            long gen = IsDefinedGeneric(out i);
-            long std = IsDefinedStandard(out i);
+            long gen = IsDefinedGeneric(out int ii);
+            long std = IsDefinedStandard(out ii);
 
             // test
-            gen = IsDefinedGeneric(out i);
-            std = IsDefinedStandard(out i);
+            gen = IsDefinedGeneric(out ii);
+            std = IsDefinedStandard(out ii);
 
             Assert.IsTrue(std > gen);
+
+            long IsDefinedGeneric(out int i)
+            {
+                var watch = Stopwatch.StartNew();
+
+                bool isDefined = true;
+
+                var isDefinedFunc =
+
+                i = 0;
+                foreach (byte val in EnumUtil<Int32Enum>.GetValues())
+                {
+                    for (i = 0; i < 200000; i++)
+                    {
+                        isDefined &= EnumUtil<Int32Enum>.IsDefined(val);
+                    }
+                }
+
+                watch.Stop();
+                Assert.IsTrue(isDefined);
+                return watch.ElapsedMilliseconds;
+            }
+
+            long IsDefinedStandard(out int i)
+            {
+                var watch = Stopwatch.StartNew();
+
+                bool isDefined = true;
+
+                i = 0;
+                foreach (int val in EnumUtil<Int32Enum>.GetValues())
+                {
+                    for (i = 0; i < 200000; i++)
+                    {
+                        isDefined &= Enum.IsDefined(typeof(Int32Enum), val);
+                    }
+                }
+
+                watch.Stop();
+                Assert.IsTrue(isDefined);
+                return watch.ElapsedMilliseconds;
+            }
         }
 
         [TestMethod]
         public void TestIsDefinedEnumPerformance()
         {
             // warmup
-            long gen = IsDefinedGenericEnum(out int i);
-            long std = IsDefinedStandardEnum(out i);
+            long gen = IsDefinedGenericEnum(out int ii);
+            long std = IsDefinedStandardEnum(out ii);
 
             // test
-            gen = IsDefinedGenericEnum(out i);
-            std = IsDefinedStandardEnum(out i);
+            gen = IsDefinedGenericEnum(out ii);
+            std = IsDefinedStandardEnum(out ii);
 
             Assert.IsTrue(std > gen);
-        }
 
-        public static long IsDefinedStandardEnum(out int i)
-        {
-            var watch = Stopwatch.StartNew();
-
-            bool isDefined = true;
-
-            i = 0;
-            foreach (var val in EnumUtil<Int32Enum>.GetValues())
+            long IsDefinedGenericEnum(out int i)
             {
-                for (i = 0; i < 200000; i++)
+                var watch = Stopwatch.StartNew();
+
+                bool isDefined = true;
+                i = 0;
+                foreach (var val in EnumUtil<Int32Enum>.GetValues())
                 {
-                    isDefined &= Enum.IsDefined(typeof(Int32Enum), Int32Enum.Two);
+                    for (i = 0; i < 200000; i++)
+                    {
+                        isDefined &= EnumUtil<Int32Enum>.IsDefined(val);
+                    }
                 }
+
+                watch.Stop();
+                Assert.IsTrue(isDefined);
+                return watch.ElapsedMilliseconds;
             }
 
-            watch.Stop();
-            Assert.IsTrue(isDefined);
-            return watch.ElapsedMilliseconds;
-        }
-
-        public static long IsDefinedGenericEnum(out int i)
-        {
-            var watch = Stopwatch.StartNew();
-
-            bool isDefined = true;
-            i = 0;
-            foreach (var val in EnumUtil<Int32Enum>.GetValues())
+            long IsDefinedStandardEnum(out int i)
             {
-                for (i = 0; i < 200000; i++)
+                var watch = Stopwatch.StartNew();
+
+                bool isDefined = true;
+
+                i = 0;
+                foreach (var val in EnumUtil<Int32Enum>.GetValues())
                 {
-                    isDefined &= EnumUtil<Int32Enum>.IsDefined(val);
+                    for (i = 0; i < 200000; i++)
+                    {
+                        isDefined &= Enum.IsDefined(typeof(Int32Enum), Int32Enum.Two);
+                    }
                 }
+
+                watch.Stop();
+                Assert.IsTrue(isDefined);
+                return watch.ElapsedMilliseconds;
             }
-
-            watch.Stop();
-            Assert.IsTrue(isDefined);
-            return watch.ElapsedMilliseconds;
         }
-
-        public static long TestGeneric(out int i)
-        {
-            var watch = Stopwatch.StartNew();
-
-            FlagsEnum natural = FlagsEnum.Two | FlagsEnum.Four;
-            FlagsEnum flag = FlagsEnum.Four;
-
-            bool discardMe = false;
-            i = 0;
-            for (; i < 2000000; i++)
-            {
-                discardMe = EnumUtil<FlagsEnum>.HasFlag(natural, flag);
-            }
-
-            watch.Stop();
-            return watch.ElapsedMilliseconds;
-        }
-
-        public static long TestSpeedNatural(out int i)
-        {
-            var watch = Stopwatch.StartNew();
-
-            FlagsEnum natural = FlagsEnum.Two | FlagsEnum.Four;
-            FlagsEnum flag = FlagsEnum.Four;
-
-            bool discardMe = false;
-            i = 0;
-            for (; i < 2000000; i++)
-            {
-                discardMe = natural.HasFlag(flag);
-            }
-
-            watch.Stop();
-            return watch.ElapsedMilliseconds;
-        }
-
+        
         [TestMethod]
         public void TestHasFlagGenericSpeed()
         {
             // Test that the new generic implementation
             // is faster than the default implementation
             // of .HasFlag(...)
-
-            int i;
+            
             // warmup
-            long gn = TestGeneric(out i);
-            long nat = TestSpeedNatural(out i);
+            long gn = TestGeneric(out int ii);
+            long nat = TestSpeedNatural(out ii);
 
             // test
-            gn = TestGeneric(out i);
-            nat = TestSpeedNatural(out i);
+            gn = TestGeneric(out ii);
+            nat = TestSpeedNatural(out ii);
 
             Assert.IsTrue(nat > gn);
+
+            long TestGeneric(out int i)
+            {
+                var watch = Stopwatch.StartNew();
+
+                FlagsEnum natural = FlagsEnum.Two | FlagsEnum.Four;
+                FlagsEnum flag = FlagsEnum.Four;
+
+                bool discardMe = false;
+                i = 0;
+                for (; i < 2000000; i++)
+                {
+                    discardMe = EnumUtil<FlagsEnum>.HasFlag(natural, flag);
+                }
+
+                watch.Stop();
+                return watch.ElapsedMilliseconds;
+            }
+
+            long TestSpeedNatural(out int i)
+            {
+                var watch = Stopwatch.StartNew();
+
+                FlagsEnum natural = FlagsEnum.Two | FlagsEnum.Four;
+                FlagsEnum flag = FlagsEnum.Four;
+
+                bool discardMe = false;
+                i = 0;
+                for (; i < 2000000; i++)
+                {
+                    discardMe = natural.HasFlag(flag);
+                }
+
+                watch.Stop();
+                return watch.ElapsedMilliseconds;
+            }
         }
 
         [TestMethod]
